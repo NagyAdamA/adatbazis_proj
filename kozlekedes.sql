973e44657ab779efd881a80a31c7f038354a7ac7
NagyAdamA <n.adam@ckik.hu>
2024-02-27 11:45:43 +0100

    táblázat

---
+++ kozlekedes.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE Közlekedes;
+
+CREATE TABLE Jármű (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    típus VARCHAR(10) CHECK(típus IN ('HÉV', 'Busz', 'Trolibusz', 'Metró', 'Villamos')),
+    gyártási_év DATE,
+    gyártmány VARCHAR(20),
+    üzemeltető VARCHAR(7) CHECK(üzemeltető IN ('BKK', 'MÁV-HÉV')),
+    akadalymentesitett BOOLEAN
+);
+
+CREATE TABLE Megálló(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    név VARCHAR(50),
+    akadalymentesitett BOOLEAN
+);
+
+CREATE TABLE Közlekedes(
+    járatszam VARCHAR(4),
+    dátum DATE,
+    indul TIME,
+    érkezik TIME
+    járműId INT,
+    megállóId INT,
+    FOREIGN KEY járműId REFERENCES járműId(id),
+    FOREIGN KEY megállóId REFERENCES Megálló(id)
+);
